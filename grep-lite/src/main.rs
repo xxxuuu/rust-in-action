43860ce9913664827bb662cdab25f93389e430dd
XUQING <751736277@qq.com>
2022-10-01 16:09:11 +0800

    feat(ch2): grep-lite

---
+++ grep-lite/src/main.rs
@@ -0,0 +1,47 @@
+extern crate core;
+
+use std::fs::File;
+use std::io;
+use std::io::{BufRead, BufReader};
+use clap::{App, Arg};
+use regex::Regex;
+
+fn process_line<T: BufRead + Sized>(reader: T, re: Regex) {
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+        match re.find(&line) {
+            Some(_) => println!("{}", line),
+            None => {}
+        }
+    }
+}
+
+fn main() {
+    let args = App::new("grep-lite")
+        .version("0.1")
+        .about("searches for patterns")
+        .arg(Arg::with_name("pattern")
+            .help("The pattern to search for")
+            .takes_value(true)
+            .required(true))
+        .arg(Arg::with_name("input")
+            .help("File to search")
+            .takes_value(true)
+            .required(false))
+        .get_matches();
+
+    let pattern = args.value_of("pattern").unwrap();
+    let re = Regex::new(pattern).unwrap();
+
+    let input = args.value_of("input").unwrap_or("-");
+
+    if input == "-" {
+        let stdin = io::stdin();
+        let reader = stdin.lock();
+        process_line(reader, re);
+    } else {
+        let f = File::open(input).unwrap();
+        let reader = BufReader::new(f);
+        process_line(reader, re);
+    }
+}
